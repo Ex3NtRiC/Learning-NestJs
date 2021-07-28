48c110d6682355f429ebc70c546169bc08ab2df9
Laith <laith.marzouka@gmail.com>
2021-07-28 15:12:07 +0300

    Adding User Module -basic-

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './users/user.module';
 
 @Module({
   imports: [
     ProductsModule,
+    UserModule,
     MongooseModule.forRoot(
       'mongodb+srv://laith_marzouka:JVo97OMDgSjHpkkT@cluster0.ti4dt.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
     ),
