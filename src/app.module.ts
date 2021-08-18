4e280d81e3ab3fda263e1a5126df6075f3eda4cc
Laith Marzouka <50340340+Ex3NtRiC@users.noreply.github.com>
2021-08-18 22:39:44 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { UserModule } from './users/user.module';
     ProductsModule,
     UserModule,
     MongooseModule.forRoot(
-      'mongodb+srv://laith_marzouka:JVo97OMDgSjHpkkT@cluster0.ti4dt.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+      'Mongo-URI',
     ),
   ],
   controllers: [AppController],
